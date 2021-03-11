ff9a1143f0944e7be958b01af536e96bba70d096
itsdivyam <divyamsrvstv@gmail.com>
2021-03-11 21:06:28 +0530

    changes in config

--- api/models/Scrap.js
+++ api/models/Scrap.js
@@ -2,11 +2,21 @@
 module.exports = {
   attributes: {
     image : {
-      type : string,
+      type : 'string',
       required : true
     },
-    location:{
-      
+    location: {
+      type: 'json',
+      columnType: 'array',
+      required: true
+      // index: '2d',
+      // longitude: {
+      //     type: 'float',
+      //     required: true,
+      // },
+      // latitude: {
+      //     type: 'float'
+      // }
     }
-  },
-};
+  }
+}
--- config/routes.js
+++ config/routes.js
@@ -12,11 +12,7 @@ module.exports.routes = {
 
   '/': { view: 'pages/homepage' },
 
-<<<<<<< HEAD
   'POST /user/signUpUser' : {
-=======
-  'GET /abc' : {
->>>>>>> 3c5f66f820723da3365ebc7a053b092809f7ad1b
     controller: 'UserController',
     action : 'signUpUser'
   }
